377fce157151c022bde4e99a6063146354c95444
MulBiNeul <hello88bgm@gmail.com>
2025-05-26 23:47:36 +0900

    Update [python]2447.py

--- 재귀/[python]2447.py
+++ 재귀/[python]2447.py
@@ -2,3 +2,19 @@
 
 import sys
 
+N = int(sys.stdin.readline())
+
+# recursive function
+def star(n):
+    if n == 3:
+        return ['***', '* *', '***']
+
+    arr = star(n//3)
+    stars = []
+    for i in arr:
+        stars.append(i*3)
+        for i in arr:
+            stars.append(i + ' '*(n//3) + i)
+    return stars
+
+print('\n'.join(star(N)))
